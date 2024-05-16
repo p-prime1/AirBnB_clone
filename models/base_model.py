3cbcdf2fda1d12050ec5eb2fb686625af273f9fe
p-prime1 <preciousnwaka95@gmail.com>
2024-05-16 16:09:28 +0100

    Added the base model class

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import cmd
+import datetime
+import uuid
+
+"""Base model in which all other classes inherits from"""
+
+class BaseModel(cmd.Cmd):
+    """Initializes all the argyments"""
+    def __init__(self):
+        """ Initializes the public instance attributes
+        Args:
+            line (str): The additional arguments passed to the command line
+            id: Assign a unique id when an instance is created
+            created_at: Assigns the current datetime when an instance is created
+            updated_at: Assigns the current dattime and updates every time an object is changed
+            """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime
+        self.updated_at = datetime.datetime
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id} {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.datetime
+
+    def to_dict(self):
+        """Returns a dictionary containing all kys/valued of __dic__ of the instance"""
+        pass
+
+
