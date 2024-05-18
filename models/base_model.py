0691a61b1eb982a0cdf75c6584814145beccbaa7
p-prime1 <preciousnwaka95@gmail.com>
2024-05-18 22:46:27 +0100

    Modified the class to create base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ import uuid
 
 class BaseModel:
     """Initializes all the argyments"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes the public instance attributes
         Args:
             line (str): The additional arguments passed to the command line
@@ -17,9 +17,15 @@ class BaseModel:
             created_at: Assigns the current datetime
             updated_at: Assigns the current dattime
             """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns an official string rep of the class"""
@@ -32,4 +38,7 @@ class BaseModel:
 
     def to_dict(self):
         """Returns a dictionary"""
-        return (self.__dict__.copy())
+        dict_copy = self.__dict__.copy()
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return (dict_copy)
