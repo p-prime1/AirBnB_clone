f2f7772a7851f29a40bccff0e02e56553115c3de
p-prime1 <preciousnwaka95@gmail.com>
2024-05-21 23:05:14 +0100

    Added a new cmment to exolain the code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,6 +55,7 @@ class FileStorage(BaseModel):
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
             # TODO: should this overwrite or insert?
+            # This implementation currently overwrites it
             FileStorage.__objects = obj_dict
 
     def attributes(self):
